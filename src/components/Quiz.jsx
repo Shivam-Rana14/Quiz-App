61f1a3e2230879b8f822ed4c648874636b01b3a1
Shivam Rana <shivamrana.cpp@gmail.com>
2025-03-11 02:06:13 +0530

    shuffle-ans-options

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,16 +1,20 @@
 import { useContext } from "react";
 import { quizContext } from "../store/quiz-context";
-import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
-  const { activeQuestionIndex, handleSelectAnswer } = useContext(quizContext);
+  const {
+    activeQuestionIndex,
+    handleSelectAnswer,
+    shuffledAnswers,
+    QUESTIONS,
+  } = useContext(quizContext);
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
--- src/store/quiz-context.jsx
+++ src/store/quiz-context.jsx
@@ -1,15 +1,21 @@
 import { createContext, useState } from "react";
+import QUESTIONS from "../questions.js";
 
 export const quizContext = createContext({
   activeQuestionIndex: 0,
   userAnswers: [],
   QUESTIONS: "",
+  shuffledAnswers: [],
+  QUESTIONS: {},
   handleSelectAnswer: () => {},
 });
 
 export default function QuizContextProvider({ children }) {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -21,6 +27,8 @@ export default function QuizContextProvider({ children }) {
   const quizCtx = {
     activeQuestionIndex: activeQuestionIndex,
     userAnswers: userAnswers,
+    shuffledAnswers: shuffledAnswers,
+    QUESTIONS: QUESTIONS,
     handleSelectAnswer: handleSelectAnswer,
   };
   return (
